c5ac441517f8db711336b34029c2b7390bec6853
Alex Solohub <le03grande@gmail.com>
2023-02-23 10:10:28 +0200

    oooooo

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -5,7 +5,7 @@ import useCardCreate from "../../app/hooks/useCardCreate";
 
 const Modal = ({setModal, modal}) => {
 
-    const {mutateAsync: cardCreate} = useCardCreate()
+    const {isLoading, mutateAsync: cardCreate} = useCardCreate()
     const wrapperRef = useRef(null)
 
     const onCreateHandler = async () => {
@@ -21,29 +21,46 @@ const Modal = ({setModal, modal}) => {
     }
 
     return (
-        <div ref={wrapperRef} className={styles.wrapper} onClick={(e) => {
-            if (e.target === wrapperRef.current)
-            setModal({
-                ...modal,
-                active: false
-            })
-        }}>
-            <div className={styles.container}>
-                <input type="text" value={modal.task} onChange={(e) => setModal({
-                    ...modal,
-                    task: e.target.value
-                })
-                }/>
-                <div className={styles.buttons}>
-                    <button onClick={onCreateHandler}>Create</button>
-                    <button onClick={() => setModal({
-                        ...modal,
-                        active: false
-                    })}>Close</button>
-                </div>
-            </div>
-        </div>
-    );
+			<div
+				ref={wrapperRef}
+				className={styles.wrapper}
+				onClick={(e) => {
+					if (e.target === wrapperRef.current)
+						setModal({
+							...modal,
+							active: false,
+						})
+				}}
+			>
+				<div className={styles.container}>
+					<input
+						type='text'
+						value={modal.task}
+						onChange={(e) =>
+							setModal({
+								...modal,
+								task: e.target.value,
+							})
+						}
+					/>
+					<div className={styles.buttons}>
+						<button disabled={isLoading} onClick={onCreateHandler}>
+							Create
+						</button>
+						<button
+							onClick={() =>
+								setModal({
+									...modal,
+									active: false,
+								})
+							}
+						>
+							Close
+						</button>
+					</div>
+				</div>
+			</div>
+		)
 };
 
 export default Modal;
