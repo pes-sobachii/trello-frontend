aee031d6da4a19905175ccd04d8a5b3b027c9655
Alex Solohub <le03grande@gmail.com>
2023-02-23 09:55:25 +0200

    llllll

--- src/app/services/lists.service.js
+++ src/app/services/lists.service.js
@@ -6,34 +6,31 @@ axios.defaults.baseURL = API_URL
 
 export const BoardService = {
     async deleteList(id) {
-        await axios.delete(`${process.env.REACT_APP_API_URL}/list/${id}`)
+        await axios.delete(`/list/${id}`);
     },
     async emptyList(id) {
-        await axios.delete(`${process.env.REACT_APP_API_URL}/list-items/${id}`)
+        await axios.delete(`/list-items/${id}`);
     },
     async deleteCard(id) {
-        await axios.delete(`${process.env.REACT_APP_API_URL}/card/${id}`)
+        await axios.delete(`/card/${id}`);
     },
     async addList(title) {
-        await axios.post(`${process.env.REACT_APP_API_URL}/list`, {
-					title,
-				})
+        await axios.post(`/list`, {
+            title
+        });
     },
     async getLists() {
-        return await axios.get(`${process.env.REACT_APP_API_URL}/lists`)
+        return await axios.get(`/lists`);
     },
     async setCard(list, title) {
-        await axios.post(`${process.env.REACT_APP_API_URL}/card`, {
-					list,
-					title,
-				})
+        await axios.post(`/card`, {
+            list, title
+        })
     },
     async updateCard(id, destinationId, sourceId) {
-        await axios.put(`${process.env.REACT_APP_API_URL}/card`, {
-					id,
-					destinationId,
-					sourceId,
-				})
+        await axios.put(`/card`, {
+            id, destinationId, sourceId
+        })
     },
 
 }
